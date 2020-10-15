86ebc4bce611cad0ddff56d9a4e418261df187ca
nelsonsantos22 <nelsonsilvasantos@outlook.com>
2020-10-15 14:58:19 +0100

    add form that changes top and bottom text state

--- src/components/MemeGenerator.js
+++ src/components/MemeGenerator.js
@@ -10,21 +10,50 @@ class MemeGenerator extends Component {
             randomImgs : "http://i.imgflip.com/1bij.jpg",
             allMemeImgs : []
         }
+        this.handleChange = this.handleChange.bind(this)
+    }
+
+    handleChange(event) {
+        const {name , value} = event.target
+        this.setState ({
+            [name] : value
+        })
     }
 
     componentDidMount() {
         fetch("https://api.imgflip.com/get_memes")
             .then(response => response.json())
-            .then(data =>
+            .then(response => {
+                /* here i'm making the same stuff as the form exercise to avoid to write response.data.memes
+                *  every time i want to refer to memes array, like so i only have to write memes[0] to acess
+                *  to the first position of the array
+                */
+                const {memes} = response.data
+                console.log(memes[0])
                 this.setState({
-                    allMemeImgs : data
-                }))
+                    allMemeImgs : memes
+                })
+            })
     }
 
     render() {
         console.log(this.state.allMemeImgs)
         return (
-            <h1>Meme Generator</h1>
+            <form className="meme-form">
+                <input 
+                    name = "topText"
+                    onChange = {this.handleChange}
+                    value = {this.state.topText}
+                    placeholder = "Top Text" 
+                />
+                 <input 
+                    name = "bottomText"
+                    onChange = {this.handleChange}
+                    value = {this.state.bottomText}
+                    placeholder = "Bottom Text" 
+                />
+                <button>Gen</button>
+            </form>
         )
     }
 }
